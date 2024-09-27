0f99cbbda57305805a1611528e5fae474d14863b
RAJ <alli-shashidhar.raj@capgemini.com>
2024-09-27 19:29:42 +0530

    dev2

--- src/com/begi/practice/GitSample.java
+++ src/com/begi/practice/GitSample.java
@@ -3,7 +3,7 @@ package com.begi.practice;
 public class GitSample {
 
 	public static void main(String[] args) {
-		int i =10;
-		System.out.println(i);
+		String s = "abc";
+		System.out.println();
 	}
 }
